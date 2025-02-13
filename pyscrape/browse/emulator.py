0a06de43b61f7782647c3d489bbecd1bbec50a6f
Daniel Hollarek <daniel.hollarek@googlemail.com>
2025-02-13 12:01:40 +0100

    browse: Added textinputs retrieval

--- pyscrape/browse/emulator.py
+++ pyscrape/browse/emulator.py
@@ -6,11 +6,12 @@ from markdownify import markdownify
 from selenium.webdriver import Keys
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webelement import WebElement
+
 
 # -----------------------------------------------
 
 class BrowserEmulator:
-
     def __init__(self):
         chrome_options = Options()
         chrome_options.add_argument(f"--load-extension={os.path.dirname(__file__)}/cookie_blocker")
@@ -20,13 +21,12 @@ class BrowserEmulator:
         self.driver.get(url)
 
     def type(self, text_box_idx : int, content : str):
-        text_inputs = self.driver.find_elements(By.CSS_SELECTOR, 'input[type="text"]')
-        visible_text_inputs = [input_box for input_box in text_inputs if input_box.is_displayed()]
+        visible_text_inputs = self.get_textinputs(visible_only=True)
         target_box = visible_text_inputs[text_box_idx]
         target_box.send_keys(content + Keys.RETURN)
 
     # ------------------------------------
-    # get current content
+    # get
 
     def get_markdown(self) -> str:
         md = markdownify(self.get_html())
@@ -43,6 +43,12 @@ class BrowserEmulator:
             return str(soup)
 
         return html_code
+
+    def get_textinputs(self, visible_only : bool) -> list[WebElement]:
+        text_inputs = self.driver.find_elements(By.CSS_SELECTOR, 'input[type="text"]')
+        if visible_only:
+            text_inputs = [input_box for input_box in text_inputs if input_box.is_displayed()]
+        return text_inputs
 
     def __del__(self):
         self.driver.quit()
@@ -65,8 +71,6 @@ class BrowserEmulator:
 
 
 
-
-
 if __name__ == "__main__":
     w1 = "https://docs.ros.org/en/foxy/index.html"
     w2 = 'https://platform.openai.com/docs/libraries#community-libraries'
@@ -74,5 +78,5 @@ if __name__ == "__main__":
     w4 = 'https://stackoverflow.com/questions/16731115/how-to-debug-a-python-segmentation-fault'
 
     be = BrowserEmulator()
-    be.visit(url=w3)
+    be.visit(url=w1)
     print(be.get_markdown())
